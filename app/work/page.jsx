2f7eea55dc3154a56c3d3a196759254156457695
Ahmadkaissoon <ahmad.kaissoon@gmail.com>
2025-06-12 22:13:42 +0300

    bug fixes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -16,8 +16,6 @@ import Image from "next/image";
 import Link from "next/link";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
 import microTech_Banner from "@/assets/assets/work/microtech/microTech_Banner.png";
-import fstWork from "@/assets/assets/work/thumb1.png";
-import sndWork from "@/assets/assets/work/thumb2.png";
 
 const Projects = [
   {
@@ -25,7 +23,7 @@ const Projects = [
     category: "Portfolio",
     title: "MicroTech co",
     description:
-      "Website that you can use to build your custom website!, with variant components and customizable navbar, footer, and SEO.",
+      "Website that you can use to build your custom website!, with variant components and customizable navbar, footer, and SEO. And This site was built by our website",
     stack: [
       { name: "Figma" },
       { name: "NextJs" },
@@ -41,25 +39,6 @@ const Projects = [
     live: "https://tech-solutions-site.netlify.app/",
     github: "",
   },
-  // {
-  //   num: "02",
-  //   category: "Accounting",
-  //   title: "Industrial Accounting website",
-  //   description:
-  //     " Industrial Accounting website. It contains all sections related to industrial accounting, including invoices, receipts, reports, etc..., in addition to other administrative and communication sections such as permissions, chat, email, and many other sections.",
-  //   stack: [
-  //     { name: "ReactJs" },
-  //     { name: "Html" },
-  //     { name: "Css" },
-  //     { name: "Tailwind" },
-  //     { name: "Javascript" },
-  //     { name: "PHP" },
-  //     { name: "Laravel" },
-  //   ],
-  //   image: "",
-  //   live: "",
-  //   github: "",
-  // },
 ];
 
 const Work = () => {
@@ -69,6 +48,7 @@ const Work = () => {
     const currentIndex = swiper.activeIndex;
     setProject(Projects[currentIndex]);
   };
+
   return Projects.length > 0 ? (
     <motion.section
       initial={{ opacity: 0 }}
@@ -81,26 +61,17 @@ const Work = () => {
       <div className="container mx-auto overflow-hidden">
         <div className="flex flex-col xl:flex-row xl:gap-[30px] w-full overflow-hidden">
           <div
-            className=" xl:h-[460px] 
-          flex flex-col xl:justify-between order-2 xl:w-[50%] xl:order-none  "
+            className="xl:h-[80vh] max-h-[80vh] overflow-y-auto pr-2
+            flex flex-col xl:justify-between order-2 xl:w-[50%] xl:order-none"
           >
-            <div className="flex flex-col  gap-[30px] h-[50%] ">
-              <div
-                className="text-8xl leading-none font-extrabold
-              text-transparent text-outline"
-              >
+            <div className="flex flex-col gap-[30px]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project?.num}
               </div>
-              <h2
-                className="text-[42px] font-bold leading-none text-white
-              group-hover:text-accent transition-all duration-500 capitalize"
-              >
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
                 {project?.category} project
               </h2>
-              <h2
-                className="text-[24px] font-semibold leading-none text-accent
-              group-hover:text-accent transition-all duration-500 capitalize"
-              >
+              <h2 className="text-[24px] font-semibold leading-none text-accent group-hover:text-accent transition-all duration-500 capitalize">
                 {project?.title}
               </h2>
               <p className="text-white/60">{project?.description}</p>
@@ -115,16 +86,14 @@ const Work = () => {
                 })}
               </ul>
               <div className="border border-white/20"></div>
-              <div className="flex items-center gap-4">
-                {project?.live != null &&
-                project?.live != undefined &&
-                project?.live != "" ? (
-                  <Link href={project?.live} target="_blank">
+              <div className="flex items-center gap-4 mb-4">
+                {project?.live && (
+                  <Link href={project.live} target="_blank">
                     <TooltipProvider delayDuration={100}>
                       <Tooltip>
                         <TooltipTrigger
                           className="w-[70px] h-[70px] rounded-full
-                      bg-white/5 cursor-pointer flex justify-center items-center group"
+                          bg-white/5 cursor-pointer flex justify-center items-center group"
                         >
                           <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
                         </TooltipTrigger>
@@ -134,16 +103,14 @@ const Work = () => {
                       </Tooltip>
                     </TooltipProvider>
                   </Link>
-                ) : null}
-                {project?.github != null &&
-                project?.github != undefined &&
-                project?.github != "" ? (
-                  <Link href={project?.github}>
+                )}
+                {project?.github && (
+                  <Link href={project.github} target="_blank">
                     <TooltipProvider delayDuration={100}>
                       <Tooltip>
                         <TooltipTrigger
                           className="w-[70px] h-[70px] rounded-full
-                      bg-white/5 cursor-pointer flex justify-center items-center group"
+                          bg-white/5 cursor-pointer flex justify-center items-center group"
                         >
                           <BsGithub className="text-white text-3xl group-hover:text-accent" />
                         </TooltipTrigger>
@@ -153,24 +120,24 @@ const Work = () => {
                       </Tooltip>
                     </TooltipProvider>
                   </Link>
-                ) : null}
+                )}
               </div>
             </div>
           </div>
+
           <div className="xl:w-[50%] overflow-hidden">
             <Swiper
               spaceBetween={30}
               slidesPerView={1}
-              className="xl:h-[520px] mb-12"
+              className="xl:h-[80vh] mb-12"
               onSlideChange={handleSlideChange}
             >
               {Projects?.map((item, index) => {
-                console.log(item);
                 return (
                   <SwiperSlide key={index}>
                     <div
-                      className="h-[460px] relative group flex justify-center
-                    items-center bg-primary"
+                      className="h-[60vh] xl:h-[70vh] relative group flex justify-center
+                      items-center bg-primary"
                     >
                       <div className="absolute top-0 bottom-0 w-full h-full z-10"></div>
                       <div className="relative w-full h-full">
@@ -187,7 +154,7 @@ const Work = () => {
               })}
               <WorkSliderBtns
                 containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-                btnStyles="bg-accent  cursor-pointer text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                btnStyles="bg-accent cursor-pointer text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
               />
             </Swiper>
           </div>
